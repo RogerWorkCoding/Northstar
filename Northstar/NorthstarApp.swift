3bb05d45583b8b33d9f06d2b3c7efcbe327c811b
rogwar51 <rogwar51@gmail.com>
2026-09-23 20:22:03 -0700

    Initial Commit

---
+++ Northstar/NorthstarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NorthstarApp.swift
+//  Northstar
+//
+//  Created by Roger Ward on 9/23/26.
+//
+
+import SwiftUI
+
+@main
+struct NorthstarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
